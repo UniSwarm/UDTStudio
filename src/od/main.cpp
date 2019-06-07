dbe218261a5aa13f1b938de5081dd172a7857c60
almontorie <alexis.montorier@laposte.net>
2019-06-07 10:07:06 +0200

    [OD] added command line arguments

--- src/od/main.cpp
+++ src/od/main.cpp
@@ -10,12 +10,33 @@
  * @brief main
  * @return
  */
-int main()
+int main(int argc, char *argv[])
 {
+    QCoreApplication app(argc, argv);
+    QCoreApplication::setApplicationName("OD");
+    QCoreApplication::setApplicationVersion("1.0");
+
+    QTextStream out(stdout);
+
+    QCommandLineParser cliParser;
+    cliParser.setApplicationDescription(QCoreApplication::translate("main", "Object Dicitonary command line interface."));
+    cliParser.addHelpOption();
+    cliParser.addVersionOption();
+    cliParser.addPositionalArgument("file", QCoreApplication::translate("main", "Source file to extract pins (pdf)."), "file");
+    cliParser.process(app);
+
+    const QStringList files = cliParser.positionalArguments();
+    if (files.isEmpty())
+    {
+        out << "error (1): input file is needed" << endl;
+        cliParser.showHelp(-1);
+    }
+    const QString &file = files.at(0);
+
     OD *od;
     od = new OD;
 
-    QString path("../eds/402.eds");
+    QString path(file);
 
     EdsParser parser(path);
     Generator generator("../src/test");
