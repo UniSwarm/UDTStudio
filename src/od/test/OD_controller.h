535e3cba2090eaf5a7a32375835f61ade6a0dda5
almontorie <alexis.montorier@laposte.net>
2019-06-11 10:53:13 +0200

    [od:controller] removed ; in sdo abort code definition

--- src/od/test/OD_controller.h
+++ src/od/test/OD_controller.h
@@ -4,11 +4,11 @@
 #include "OD.h"
 
 // ============ sdo abort codes =============
-#define OD_ABORT_CODE_WRITE_ONLY            0x06010001;
-#define OD_ABORT_CODE_READ_ONLY             0x06010002;
-#define OD_ABORT_CODE_NO_OBJECT             0x06020000;
-#define OD_ABORT_CODE_NO_SUBINDEX           0x06090011;
-#define OD_ABORT_CODE_LENGTH_DOESNT_MATCH   0x06070010;
+#define OD_ABORT_CODE_WRITE_ONLY            0x06010001
+#define OD_ABORT_CODE_READ_ONLY             0x06010002
+#define OD_ABORT_CODE_NO_OBJECT             0x06020000
+#define OD_ABORT_CODE_NO_SUBINDEX           0x06090011
+#define OD_ABORT_CODE_LENGTH_DOESNT_MATCH   0x06070010
 
 // =============== functions ================
 void OD_reset();
