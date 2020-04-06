9740f5ee6623a9668e3a6cb5a0481f84a1742b52
jvaquer <julien.vaquer@uniswarm.eu>
2020-04-06 18:31:45 +0200

    [ODDB] Improved manegement EDS File seraching

--- src/lib/od/db/oddb.cpp
+++ src/lib/od/db/oddb.cpp
@@ -1,9 +1,27 @@
+/**
+ ** This file is part of the UDTStudio project.
+ ** Copyright 2019-2020 UniSwarm
+ **
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **/
+
 #include "oddb.h"
 
-#include <QDirIterator>
-#include <QDebug>
 #include "parser/edsparser.h"
 #include <QCryptographicHash>
+#include <QDebug>
+#include <QDirIterator>
 
 OdDb::OdDb(QString directory)
     : _directory(directory)
@@ -35,9 +53,12 @@ void OdDb::searchFile()
         byte.append(deviceDescription->index(0x1000)->subIndex(0)->value().toByteArray());
         byte.append(deviceDescription->index(0x1018)->subIndex(1)->value().toByteArray());
         byte.append(deviceDescription->index(0x1018)->subIndex(2)->value().toByteArray());
-        byte.append(deviceDescription->index(0x1018)->subIndex(3)->value().toByteArray());
         QByteArray hash = QCryptographicHash::hash(byte, QCryptographicHash::Md4);
-        _mapFile.insert(hash, _directory + "/" + file);
+        QPair<quint32, QString> pair;
+        pair.first = deviceDescription->index(0x1018)->subIndex(3)->value().toUInt();
+        pair.second = _directory + "/" + file;
+        _mapFile.insert(hash, pair);
+
         byte.clear();
     }
 }
@@ -48,14 +69,23 @@ QString OdDb::file(quint32 deviceType, quint32 vendorID, quint32 productCode, qu
     byte.append(QVariant(deviceType).toByteArray());
     byte.append(QVariant(vendorID).toByteArray());
     byte.append(QVariant(productCode).toByteArray());
-    byte.append(QVariant(revisionNumber).toByteArray());
     QByteArray hash = QCryptographicHash::hash(byte, QCryptographicHash::Md4);
     _mapFile.contains(hash);
 
-    if (_mapFile.contains(hash))
+    QList<QPair<quint32, QString>> values = _mapFile.values(hash);
+
+    for (quint32 j = revisionNumber; j > 0; j--)
     {
-        return _mapFile.value(hash);
+        for (int i = 0; i < values.size(); i++)
+        {
+            if (values.at(i).first == j)
+            {
+                qDebug() << "deviceType :" << deviceType << ", vendorID :" << vendorID << ", productCode :" << productCode << ", revisionNumber :" << j;
+                return values.at(i).second;
+            }
+        }
     }
+
     return QString();
 }
 
--- src/lib/od/db/oddb.h
+++ src/lib/od/db/oddb.h
@@ -1,33 +1,50 @@
+/**
+ ** This file is part of the UDTStudio project.
+ ** Copyright 2019-2020 UniSwarm
+ **
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **/
+
 #ifndef ODDB_H
 #define ODDB_H
 
 #include "od_global.h"
-#include <QString>
 #include <QMap>
+#include <QString>
 
 class OD_EXPORT OdDb
 {
-  public:
+public:
     OdDb(QString directory = ".");
 
     void setDirectory(QString directory);
     QString file(quint32 deviceType, quint32 vendorID, quint32 productCode, quint32 revisionNumber);
     void refreshFile();
 
-  private:
+private:
     quint32 _deviceType;
     quint32 _vendorID;
     quint32 _productCode;
     quint32 _revisionNumber;
 
-    QMap<QByteArray, QString> _mapFile;
+    QMultiMap<QByteArray, QPair<quint32, QString>> _mapFile;
     QString _directory;
     QString _fileName;
     QString _path;
     QList<QString> _directoryList;
 
     void searchFile();
-
 };
 
 #endif // ODDB_H
