8a1a4675adb02b75479d6cd9ab849dab98716c89
sebcaux <sebastien.caux@uniswarm.eu>
2021-01-23 10:33:06 +0100

    Updated licence headers

--- src/lib/canopen/profile/p402/nodeprofile402.cpp
+++ src/lib/canopen/profile/p402/nodeprofile402.cpp
@@ -1,6 +1,6 @@
 ﻿/**
  ** This file is part of the UDTStudio project.
- ** Copyright 2019-2020 UniSwarm
+ ** Copyright 2019-2021 UniSwarm
  **
  ** This program is free software: you can redistribute it and/or modify
  ** it under the terms of the GNU General Public License as published by
