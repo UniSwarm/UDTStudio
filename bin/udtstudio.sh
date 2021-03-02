e3bfd45fbdf535f378319db2037cd1c1c07b9fa7
sebcaux <sebastien.caux@uniswarm.eu>
2021-03-02 14:49:16 +0100

    Fixed sh LD_PATH

--- bin/udtstudio.sh
+++ bin/udtstudio.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-LD_LIBRARY_PATH=/opt/Qt5.13.0/5.13.0/gcc_64/lib/ "$DIR/udtstudio"
-
+"$DIR/udtstudio" ${1+"$@"}
