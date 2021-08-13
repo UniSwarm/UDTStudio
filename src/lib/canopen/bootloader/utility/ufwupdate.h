b2a6f4cf91e1d87a224e61b1b3dbf04f525ce955
jvaquer <julien.vaquer@uniswarm.eu>
2021-08-13 09:46:56 +0200

    [canopen:bootloader] Added ufwupdate class

---
+++ src/lib/canopen/bootloader/utility/ufwupdate.h
@@ -0,0 +1,42 @@
+/**
+ ** This file is part of the UDTStudio project.
+ ** Copyright 2019-2021 UniSwarm
+ **
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#ifndef UFWUPDATE_H
+#define UFWUPDATE_H
+
+#include "canopen_global.h"
+
+#include "../parser/ufwparser.h"
+
+class CANOPEN_EXPORT UfwUpdate
+{
+public:
+    UfwUpdate();
+
+    void setUfw(const UfwParser &ufw);
+
+    int update();
+
+    int status();
+
+private:
+    UfwParser _ufw;
+
+};
+
+#endif // UFWUPDATE_H
