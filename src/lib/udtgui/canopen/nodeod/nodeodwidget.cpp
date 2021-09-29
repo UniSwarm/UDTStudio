b32d2f9147e838ef4c5b11a7fe64feeeeb972686
jvaquer <julien.vaquer@uniswarm.eu>
2021-09-29 13:38:41 +0200

    [udtgui:NodeodWidget] Added cp mode filter

--- src/lib/udtgui/canopen/nodeod/nodeodwidget.cpp
+++ src/lib/udtgui/canopen/nodeod/nodeodwidget.cpp
@@ -160,6 +160,7 @@ void NodeOdWidget::createDefaultFilters(uint profile)
         _filterCombobox->addItem(tr("402 pc (Position Control)"), QVariant("0x[6-A][08](6[2-8]|F[24AC])"));
         _filterCombobox->addItem(tr("402 pp (Profile Position)"), QVariant("0x[6-A][08](7[ABDEF]|8[0-6]|A[34]|C[56]|6[2-4])"));
         _filterCombobox->addItem(tr("402 ip (Interpolated Position)"), QVariant("0x[6-A][08](C[0-4]|7[B-F]|8[0-5]|C[5-6]|6[2-4])"));
+        _filterCombobox->addItem(tr("402 cp (Continuous Position)"), QVariant("0x[6-A][08](7[BDEF]|8[01345]|C[56]|6[2-4])|0x4[1-8]F0"));
         _filterCombobox->addItem(tr("402 pv (Profile Velocity"), QVariant("0x[6-A][08](6[9-F]|70|F[8F])"));
         _filterCombobox->addItem(tr("402 vl (Velocity)"), QVariant("0x[6-A][08]4[2-F]"));
         _filterCombobox->addItem(tr("402 dty (Duty cycle)"), QVariant("0x4[13579BDE][A-F]"));
