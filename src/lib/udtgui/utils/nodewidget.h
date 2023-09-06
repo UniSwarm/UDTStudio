45727edb98d2e6a146ba8f8e0c576425dfc1b057
sebcaux <sebastien.caux@uniswarm.eu>
2023-09-06 10:57:06 +0200

    [udtgui:utils] Fixed compilation on windows, added files licence headers

--- src/lib/udtgui/utils/nodewidget.h
+++ src/lib/udtgui/utils/nodewidget.h
@@ -1,11 +1,31 @@
+/**
+ ** This file is part of the UDTStudio project.
+ ** Copyright 2019-2021 UniSwarm
+ **
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **/
+
 #ifndef NODEWIDGET_H
 #define NODEWIDGET_H
+
+#include "udtgui_global.h"
 
 #include <QWidget>
 
 #include "canopen/indexWidget/abstractindexwidget.h"
 
-class NodeWidget : public QWidget
+class UDTGUI_EXPORT NodeWidget : public QWidget
 {
     Q_OBJECT
 public:
